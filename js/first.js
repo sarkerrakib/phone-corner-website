d0b257b4bfb2a8417e185d50d0c75ba5ea89fb3f
sarkerrakib <freelancerrakib25@gmail.com>
2022-03-02 14:36:10 +0600

    Display all mobile

--- js/first.js
+++ js/first.js
@@ -19,3 +19,45 @@ const searchMobile = () => {
         });
 
 }
+
+
+
+const errorAlert = () => {
+    const errorHandalingv = document.getElementById('error-box');
+    errorHandalingv.classList.remove('d-none')
+    document.getElementById('search-result').style.display = 'none'
+    const phoneDetails = document.getElementById('data-details');
+    phoneDetails.textContent = '';
+
+}
+
+const displaySearchResult = data => {
+    const searchResult = document.getElementById('search-result');
+    searchResult.innerHTML = ''
+    document.getElementById('data-details').innerHTML = ''
+    const errorHandalingv = document.getElementById('error-box');
+    errorHandalingv.classList.add('d-none')
+
+    document.getElementById('search-result').style.display = 'flex'
+
+    if (data.length > 20) {
+        for (let i = 0; i < 20; i++) {
+            const item = data[i];
+            items(item)
+
+        }
+
+
+
+
+    } else if (data.length <= 20) {
+        for (const item of data) {
+
+            items(item)
+
+        }
+
+    }
+
+
+}
